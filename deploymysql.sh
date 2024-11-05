e0d4d32f78d9575efd97385ad2d3522bf5c3094f
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-11-05 14:45:55 +0200

    Update deploymysql.sh

--- deploymysql.sh
+++ deploymysql.sh
@@ -3,25 +3,25 @@ set -e
 
 echo "** Installing MySQL **"
 sudo apt-get update -y
-
 sudo apt-get install -y wget lsb-release gnupg
-
 
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
 sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
 sudo apt-get update -y
 
-echo "** Installing MySQL Server **"
 sudo apt-get install -y mysql-server
 
-echo "** Downloading init.sql **"
-curl -o /tmp/init.sql https://raw.githubusercontent.com/12Reddit12/devops/refs/heads/main/init.sql
+MYSQL_ROOT_PASSWORD="todolistpswd"
 
 echo "** Starting MySQL service **"
 sudo systemctl start mysql
 
+echo "** Setting MySQL root password **"
+sudo mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+
 echo "** Initializing database **"
-sudo mysql -u root -e "CREATE DATABASE IF NOT EXISTS todolist;"
-sudo mysql -u root todolist < /tmp/init.sql
+sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS todolist;"
+curl -o /tmp/init.sql https://raw.githubusercontent.com/12Reddit12/devops/refs/heads/main/init.sql
+sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" todolist < /tmp/init.sql
 
 echo "** MySQL setup completed **"
