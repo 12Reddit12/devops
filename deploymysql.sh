2d99eeb5771b12d9b9f446c5074958436cd51adb
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-11-05 14:39:02 +0200

    Update deploymysql.sh

--- deploymysql.sh
+++ deploymysql.sh
@@ -3,7 +3,16 @@ set -e
 
 echo "** Installing MySQL **"
 sudo apt-get update -y
-sudo apt-get install -y mysql-server curl
+
+sudo apt-get install -y wget lsb-release gnupg
+
+
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
+sudo apt-get update -y
+
+
+sudo apt-get install -y mysql-server
 
 echo "** Downloading init.sql **"
 curl -o /tmp/init.sql https://raw.githubusercontent.com/12Reddit12/devops/refs/heads/main/init.sql
