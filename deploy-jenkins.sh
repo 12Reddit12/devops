7cd6d9b55a9ff3450f95b8d2ba11ea098d990080
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-11-12 13:12:02 +0200

    Create deploy-jenkins.sh

---
+++ deploy-jenkins.sh
@@ -0,0 +1,27 @@
+ #!/bin/bash
+ set -e
+sudo apt-get update
+echo "** Install Jenkins **"
+sudo apt-get install -y openjdk-11-jdk
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install -y jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+
+echo "** Install Docker **"
+sudo apt-get install -y docker.io
+sudo usermod -aG docker jenkins
+
+echo "** Installing git **"
+sudo apt-get install -y git
+
+echo "** Installing Google Cloud SDK **"
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] \
+  http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee \
+  -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update && sudo apt-get install -y google-cloud-sdk      
