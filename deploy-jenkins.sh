0eb8db46f4d9590b3094413256d62c0dcd401c80
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-11-28 13:48:08 +0200

    Update deploy-jenkins.sh

--- deploy-jenkins.sh
+++ deploy-jenkins.sh
@@ -22,9 +22,7 @@ echo "** Installing git **"
 sudo apt-get install -y git
 
 echo "** Installing Google Cloud SDK **"
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] \
-  http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee \
-  -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt-get update && sudo apt-get install -y google-cloud-sdk      
-sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
-sudo apt-get install kubectl
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get update -y && sudo apt-get install -y google-cloud-sdk      
+sudo apt-get install -y google-cloud-sdk-gke-gcloud-auth-plugin
+sudo apt-get install -y kubectl
