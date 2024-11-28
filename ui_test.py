4bd8a1503ca876b6c1c0fc0e922434cf3ac7a577
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-11-28 15:43:47 +0200

    Update ui_test.py

--- ui_test.py
+++ ui_test.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 import time
