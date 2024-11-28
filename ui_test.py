5008c2f1072e13ca5468c33f5343204f9a586b80
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-11-28 15:09:26 +0200

    Update ui_test.py

--- ui_test.py
+++ ui_test.py
@@ -9,7 +9,6 @@ import time
 def driver():
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
-    time.sleep(300)
     options.add_argument('--headless') 
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
