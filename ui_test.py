a7da8cd50870756d6df9c63f425878fafb7f5bb5
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-11-28 15:33:06 +0200

    Update ui_test.py

--- ui_test.py
+++ ui_test.py
@@ -24,6 +24,6 @@ def test_login_page(driver):
     login_button = driver.find_element(By.XPATH, "/html/body/div[1]/form/button[2]")
     username.send_keys("testuser")
     password.send_keys("testpassword")
-    login_button[1].send_keys('\n')
+    driver.execute_script("arguments[0].click();", login_button[1])
     time.sleep(3)
     assert driver.current_url == "http://localhost:1337/pages/login.php", "Failed to login"
