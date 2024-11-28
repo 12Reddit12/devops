8b76ad9decda50bf136480d7da496e6562071371
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-11-28 16:48:37 +0200

    Update deploy-jenkins.sh

--- deploy-jenkins.sh
+++ deploy-jenkins.sh
@@ -36,38 +36,15 @@ sudo pip3 install webdriver-manager
 sudo apt-get install -y chromium-chromedriver
 sudo apt install -y google-chrome-stable
 
-CHROME_DRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`
-SELENIUM_STANDALONE_VERSION=3.13.0
-SELENIUM_SUBDIR=$(echo "$SELENIUM_STANDALONE_VERSION" | cut -d"." -f-2)
 
-# Install dependencies.
 sudo apt-get update
 sudo apt-get install -y unzip openjdk-8-jre-headless xvfb libxi6 libgconf-2-4
 
-# Install pip
+
 sudo apt-get install -y python3-pip
 sudo pip3 install --upgrade pip
 sudo pip3 install selenium requests beautifulsoup4
 
-# Install Chrome.
-sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
-sudo echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get -y update
-sudo apt-get -y install chromium-browser
-
-# Install ChromeDriver.
-wget -N http://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip -P ~/
-unzip ~/chromedriver_linux64.zip -d ~/
-rm ~/chromedriver_linux64.zip
-sudo mv -f ~/chromedriver /usr/local/bin/chromedriver
-sudo chown root:root /usr/local/bin/chromedriver
-sudo chmod 0755 /usr/local/bin/chromedriver
-
-# Install Selenium.
-wget -N http://selenium-release.storage.googleapis.com/$SELENIUM_SUBDIR/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar -P ~/
-sudo mv -f ~/selenium-server-standalone-$SELENIUM_STANDALONE_VERSION.jar /usr/local/bin/selenium-server-standalone.jar
-sudo chown root:root /usr/local/bin/selenium-server-standalone.jar
-sudo chmod 0755 /usr/local/bin/selenium-server-standalone.jar
 
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
