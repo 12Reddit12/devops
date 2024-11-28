21de83a0d40a36d544daed800c4333095308e4cd
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-11-28 14:43:53 +0200

    Update ui_test.py

--- ui_test.py
+++ ui_test.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import pytest
+import time
 
 @pytest.fixture(scope="module")
 def driver():
