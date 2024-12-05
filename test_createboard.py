a6ffc34f854a3ea2ea73d5184214b63a3e50563d
12Reddit12 <46645343+12Reddit12@users.noreply.github.com>
2024-12-05 14:42:51 +0200

    Update test_createboard.py

--- test_createboard.py
+++ test_createboard.py
@@ -6,14 +6,12 @@ def test_create_board_successfully():
     """Тест на успішне створення проекту."""
     session = requests.Session()
 
-    # Логинимся
     login_response = session.post(
         "http://localhost:1337/controllers/login.php",
         data={"username": "new_user", "password": "newpassword"}
     )
     assert login_response.status_code in [200, 302], "Login should be successful"
 
-    # Создаем проект
     data = {
         "board-name": "Test Board",
         "description": "This is a test project description."
@@ -26,13 +24,11 @@ def test_create_board_missing_fields():
     """Тест на створення проекту з порожніми полями."""
     session = requests.Session()
 
-    # Логинимся
     session.post(
         "http://localhost:1337/controllers/login.php",
         data={"username": "new_user", "password": "newpassword"}
     )
 
-    # Пропускаем обязательное поле
     data = {
         "board-name": "", 
         "description": "nonvalidproject."
